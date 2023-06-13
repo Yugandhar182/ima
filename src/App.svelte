2947a4823db5285a7f21d7fe1d940fca6e29e7b0
Yugandhar182 <yugandhar@recruitly.io>
2023-06-13 22:28:06 +0530

    ,

--- src/App.svelte
+++ src/App.svelte
@@ -31,7 +31,7 @@
         caption: "Actions",
         cellTemplate: function (container, options) {
           const downloadLink = document.createElement("a");
-          downloadLink.href = `https://api.recruitly.io/api/cloudfile/download?cloudFileId=${options.data.id}&apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77`;
+          downloadLink.href = `https://api.recruitly.io/api/candidatecv/${options.data.id}?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`;
           downloadLink.target = "_blank";
           downloadLink.download = `CV_${options.data.id}.pdf`;
           downloadLink.innerText = "Download CV";
@@ -63,28 +63,9 @@
               const cvData = await cvResponse.json();
               const cvHtml = cvData.html;
               if (cvHtml) {
-                const cvPopup = document.createElement("div");
-                cvPopup.className = "cv-popup";
-
-                const cvContent = document.createElement("div");
-                cvContent.className = "cv-popup-content";
-                cvPopup.appendChild(cvContent);
-
-                const closeBtn = document.createElement("span");
-                closeBtn.className = "cv-popup-close";
-                closeBtn.innerText = "Close";
-                closeBtn.addEventListener("click", () => {
-                  cvPopup.remove();
-                });
-                cvContent.appendChild(closeBtn);
-
-                const cvFrame = document.createElement("iframe");
-                cvFrame.srcdoc = cvHtml;
-                cvFrame.style.width = "100%";
-                cvFrame.style.height = "100%";
-                cvContent.appendChild(cvFrame);
-
-                document.body.appendChild(cvPopup);
+                const cvWindow = window.open("", "_blank");
+                cvWindow.document.write(cvHtml);
+                cvWindow.document.close();
               } else {
                 alert("CV file not found.");
               }
@@ -131,7 +112,6 @@
         paging: {
           pageSize: 10,
         },
-
         onInitialized: () => {},
       }
     );
@@ -142,38 +122,8 @@
   #dataGrid {
     height: 400px;
   }
-
-  .cv-popup {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 9999;
-  }
-
-  .cv-popup-content {
-    background-color: white;
-    padding: 20px;
-    max-width: 800px;
-    max-height: 80%;
-    overflow-y: auto;
-    text-align: center;
-    position: relative;
-  }
-
-  .cv-popup-close {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    cursor: pointer;
-  }
 </style>
 
-
+<h1 style="color: blue;">Job Candidate Details</h1>
 
 <div id="dataGrid"></div>
