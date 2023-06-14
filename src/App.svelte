d970619c664b434b5c20313338b30b131acf5b50
Yugandhar182 <yugandhar@recruitly.io>
2023-06-14 23:17:01 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -5,8 +5,6 @@
 
   let jsonData = [];
   let gridData = [];
-  let showPopup = false;
-  let pdfUrl = "";
 
   onMount(async () => {
     const response = await fetch(
@@ -28,11 +26,10 @@
       { dataField: "firstName", caption: "Full Name", width: 200 },
       { dataField: "surname", caption: "Surname", width: 200 },
       { dataField: "email", caption: "Email", width: 200 },
-      { dataField: "mobile", caption: "Mobile", width: 155 },
+      { dataField: "mobile", caption: "Mobile", width: 150 },
       {
         caption: "Actions",
         cellTemplate: function (container, options) {
-          console.log(options.data);
           const link = document.createElement("a");
           link.href = `https://api.recruitly.io/api/candidatecv/${options.data.id}?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`;
           link.target = "_blank";
@@ -41,12 +38,11 @@
             event.preventDefault();
             const cvResponse = await fetch(link.href);
             if (cvResponse.ok) {
-              const blob = await cvResponse.blob();
-              const url = URL.createObjectURL(blob);
-              pdfUrl = url;
-              showPopup = true;
+              const cvBlob = await cvResponse.blob();
+              const cvUrl = URL.createObjectURL(cvBlob);
+              window.open(cvUrl, "_blank");
             } else {
-              alert("Failed to fetch CV file.");
+              alert("CV file not found.");
             }
           });
           container.appendChild(link);
@@ -99,36 +95,8 @@
   #dataGrid {
     height: 400px;
   }
-  
-  .popup {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 600px;
-    height: 800px;
-    background-color: #fff;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    padding: 20px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  }
-  
-  .popup-close {
-    position: absolute;
-    top: 5px;
-    right: 5px;
-    cursor: pointer;
-  }
 </style>
 
 <h1 style="color:blue;">Job Candidate Details</h1>
 
 <div id="dataGrid"></div>
-
-{#if showPopup}
-  <div class="popup">
-    <div class="popup-close" on:click={() => { showPopup = false; pdfUrl = ""; }}>Close</div>
-    <iframe src="{pdfUrl}" width="100%" height="100%" frameborder="0"></iframe>
-  </div>
-{/if}
