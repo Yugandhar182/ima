6b01cb00eccb400b71cb117e95c626fbf4ab5559
Yugandhar182 <yugandhar@recruitly.io>
2023-07-10 20:28:14 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,4 @@
 <script>
-	import { onMount } from 'svelte';
-  
 	const API_KEY = '548841973896839';
 	const API_SECRET = 'SMPrJaeSvS2hlxwCABPwUf6dpkE';
 	const CLOUD_NAME = 'dv6i5lgia';
@@ -8,26 +6,22 @@
 	let images = [];
   
 	async function fetchImages() {
-	  try {
-		const response = await fetch(`https://api.cloudinary.com/v1_1/${CLOUD_NAME}/resources/image`, {
-		  headers: {
-			'Authorization': 'Basic ' + btoa(`${API_KEY}:${API_SECRET}`)
-		  }
-		});
-		const data = await response.json();
-		images = data.resources;
-		console.log('Fetched images:', images);
-	  } catch (error) {
-		console.error('Error fetching images:', error);
-	  }
+	  const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.cloudinary.com/v1_1/${CLOUD_NAME}/resources/image/upload`, {
+		headers: {
+		  Authorization: `Basic ${btoa(`${API_KEY}:${API_SECRET}`)}`
+		}
+	  });
+  
+	  const data = await response.json();
+	  images = data.resources;
 	}
   
-	onMount(fetchImages);
+	fetchImages();
   </script>
   
-  <ul>
+  <main>
 	{#each images as image}
-	  <li>{image.public_id}</li>
+	  <img src={image.url} alt={image.public_id} style="width: 200px; height: 150px;" />
 	{/each}
-  </ul>
+  </main>
   
